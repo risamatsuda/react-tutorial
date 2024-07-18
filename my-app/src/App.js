5936b927ca99a77c6e91de22a914999c73d185e7
risamatsuda <165621781+risamatsuda@users.noreply.github.com>
2024-07-18 02:16:02 +0000

    タイムトラベルの追加

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,8 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board(){
-  //プレイヤーが着手するたびに、どちらが次のプレイヤーかを切り替えるための変数
-  const [xIsNext, setXIsNext] = useState(true);
-  //盤面の状態を管理するための変数
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
+function Board({xIsNext, squares, onPlay}){
   function handleClick(i){
     if(squares[i]||calculateWinner(squares)){
       return;
@@ -23,11 +19,10 @@ export default function Board(){
     }else{
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
-  
+
   const winner = calculateWinner(squares);
   let status;
   if(winner){
@@ -57,6 +52,53 @@ export default function Board(){
     </>
   );
 }
+
+export default function Game() {
+  //const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = (currentMove % 2) === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if  (move > 0){
+      description = 'Go to move #' + move;
+    }else{
+      description = 'Go to game start';
+    }    
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
 
 //「この中身はReact特有のものではないので、あまり気にしないようにしてください」
 function calculateWinner(squares){
